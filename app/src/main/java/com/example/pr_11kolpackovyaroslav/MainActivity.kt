7f383f1f7b54462dcbb64f85f07032242c0d358c
yaroslaizerone <99257936+yaroslaizerone@users.noreply.github.com>
2022-06-04 16:40:43 +0700

    End commit

--- app/src/main/java/com/example/pr_11kolpackovyaroslav/MainActivity.kt
+++ app/src/main/java/com/example/pr_11kolpackovyaroslav/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.pr_11kolpackovyaroslav
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonsa: Button
+    private lateinit var chkb_1:CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+        buttonsa = findViewById(R.id.secondActivityBtn)
+        chkb_1 = findViewById(R.id.chkb_1)
+        chkb_1.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if (isChecked){
+                Toast.makeText(applicationContext,
+                    "Вы согласились с условиями политики конфидециальности" , Toast.LENGTH_LONG).show()
+                buttonsa.setOnClickListener{
+                    val intent = Intent(this,SecondACT::class.java)
+                    startActivity(intent)
+                }
+            }else{
+                Toast.makeText(applicationContext,
+                    "Вы отказались от условий политики конфидециальности" , Toast.LENGTH_LONG).show()
+                buttonsa.setOnClickListener{
+                    val intent = 0
+            }
+        }
+    }}}
--- app/src/main/java/com/example/pr_11kolpackovyaroslav/SecondACT.kt
+++ app/src/main/java/com/example/pr_11kolpackovyaroslav/SecondACT.kt
@@ -1,11 +1,29 @@
 package com.example.pr_11kolpackovyaroslav
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondACT : AppCompatActivity() {
+    private lateinit var buttonsa: Button
+    private lateinit var buttonsa_2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        buttonsa = findViewById(R.id.butrti_1)
+        buttonsa_2 = findViewById(R.id.butrti_2)
+        buttonsa.setOnClickListener{
+            val intent = Intent(this,tried::class.java)
+            startActivity(intent)
+        }
+        buttonsa_2.setOnClickListener{
+            val intent = Intent(this,tried::class.java)
+            startActivity(intent)
+        }
+        val actionBar = supportActionBar
+        actionBar!!.title = "Страница 2"
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
+
 }
